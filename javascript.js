ef8c28a3972f552b1149f7c9a44b409ccb2e977b
Taciano <taciano3003@gmail.com>
2023-01-05 21:11:01 -0300

    Finished the js code. No bugs yet.

--- javascript.js
+++ javascript.js
@@ -6,12 +6,40 @@
 
 function getComputerChoice() {
     let numberChoice = Math.floor(Math.random() * 3);
-    if (numberChoice == 0) {
+    return numberChoice;
+}
+
+function computerChoiceToString(number) {
+    if (number == 0) {
         return 'Rock';
-    } else if (numberChoice == 1) {
+    } else if (number == 1) {
         return 'Paper';
     } else {
         return 'Scissors';
+    }
+}
+
+function getUserChoiceString() {
+    let userChoiceString;
+    let condition = true;
+    while (condition) {
+        userChoiceString = prompt('Input your choice: ');
+        userChoiceString = userChoiceString.toLowerCase();
+        if ((userChoiceString == 'rock') || (userChoiceString == 'paper') || (userChoiceString == 'scissors')) {
+            !condition;
+            return userChoiceString;
+        }
+    }
+}
+function userChoiceToNumber(userChoiceString) {
+    if (userChoiceString == 'rock') {
+        return 0;
+    }
+    else if (userChoiceString == 'paper') {
+        return 1;
+    }
+    else {
+        return 2;
     }
 }
 
@@ -34,4 +62,29 @@ function playRound(computerChoice, computerChoiceString, playerChoice, playerCho
     }
 }
 
-playRound(1, 'Paper', 0, 'Rock');
+function game() {
+    let computerChoice, computerChoiceString, userChoiceString, userChoice;
+    let userPoints = 0;
+    let computerPoints = 0;
+    for (i = 0; i < 5; i++) {
+        computerChoice = getComputerChoice();
+        computerChoiceString = computerChoiceToString(computerChoice);
+        userChoiceString = getUserChoiceString();
+        userChoice = userChoiceToNumber(userChoiceString);
+        let roundResult = playRound(computerChoice, computerChoiceString, userChoice, userChoiceString);
+        if (roundResult == 1) {
+            userPoints++;
+        } else if (roundResult == -1) {
+            computerPoints++;
+        }
+    }
+    if (computerPoints > userPoints) {
+        console.log('Computer wins ' + computerPoints + ' to ' + userPoints + '.');
+    } else if (userPoints > computerPoints) {
+        console.log('User wins ' + userPoints + ' to ' + computerPoints + '.');
+    } else {
+        console.log('Tie ' + userPoints + ' to ' + computerPoints + '.');
+    }
+}
+
+game();
