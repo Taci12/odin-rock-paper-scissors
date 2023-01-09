e5f9c71eed22dabc9077c2a5b9744881e49267cc
Taciano <taciano3003@gmail.com>
2023-01-09 16:15:49 -0300

    - Implementation of checkWinner - Updated buttons -Changed the order of the html elements

--- javascript.js
+++ javascript.js
@@ -3,15 +3,20 @@ const paperButton = document.querySelector('#paperButton');
 const scissorsButton = document.querySelector('#scissorsButton');
 const scoreDiv = document.querySelector('#score-div');
 const outcomeDiv = document.querySelector('#outcome-div');
+const scores = {
+    computerScore: 0,
+    userScore: 0
+};
+
 
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
-    let numberChoice = Math.floor(Math.random() * 3);
+    let number = Math.floor(Math.random() * 3);
     return choices[number];
 }
 
-function playRound(userChoice, computerChoice) {
-    if (computerChoice === userCoice) {
+function playRound(userChoice, computerChoice, scores) {
+    if (computerChoice === userChoice) {
         let p = document.createElement('p');
         p.innerText = `Tie! Both picked ${userChoice}`;
         outcomeDiv.appendChild(p);
@@ -19,26 +24,32 @@ function playRound(userChoice, computerChoice) {
         let p = document.createElement('p');
         p.innerText = `Computer wins! ${computerChoice} beats ${userChoice}`;
         outcomeDiv.appendChild(p);
+        scores.computerScore++;
     } else if ((userChoice === 'Rock') && (computerChoice === 'Scissors')) {
         let p = document.createElement('p');
         p.innerText = `User wins! ${userChoice} beats ${computerChoice}`;
         outcomeDiv.appendChild(p);
+        scores.userScore++
     } else if ((userChoice === 'Paper') && (computerChoice === 'Scissors')) {
         let p = document.createElement('p');
         p.innerText = `Computer wins! ${computerChoice} beats ${userChoice}`;
         outcomeDiv.appendChild(p);
+        scores.computerScore++;
     } else if ((userChoice === 'Paper') && (computerChoice === 'Rock')) {
         let p = document.createElement('p');
         p.innerText = `User wins! ${userChoice} beats ${computerChoice}`;
         outcomeDiv.appendChild(p);
+        scores.userScore++;
     } else if ((userChoice === 'Scissors') && (computerChoice === 'Rock')) {
         let p = document.createElement('p');
         p.innerText = `Computer wins! ${computerChoice} beats ${userChoice}`;
         outcomeDiv.appendChild(p);
+        scores.computerScore++;
     } else if ((userChoice === 'Scissors') && (computerChoice === 'Paper')) {
         let p = document.createElement('p');
         p.innerText = `User wins! ${userChoice} beats ${computerChoice}`;
         outcomeDiv.appendChild(p);
+        scores.userScore++;
     }
 }
 
@@ -56,3 +67,22 @@ scissorsButton.addEventListener('click', () => {
     let computerChoice = getComputerChoice();
     playRound('Scissors', computerChoice);
 });
+
+
+function checkWinner(scores, outcomeDiv) {
+    if (scores.computerScore === 5) {
+        let h3 = document.createElement('h3');
+        h3.innerText = `Too bad! Computer won ${scores.computerScore} to ${scores.userScore}`;
+        h3.style.color = 'red';
+        outcomeDiv.appendChild(h3);
+    } else if (scores.userScore === 5) {
+        let h3 = document.createElement('h3');
+        h3.innerText = `Congratulations! You won ${scores.userScore} to ${scores.computerScore}`;
+        h3.style.color = 'green';
+        outcomeDiv.appendChild(h3);
+    };
+
+};
+
+playGame(scores);
+console.log(scores.computerScore);
